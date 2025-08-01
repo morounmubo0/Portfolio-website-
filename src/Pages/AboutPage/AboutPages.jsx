d3ba15206137407c4a461a0144fd11f19b424b8e
Icedev <moromubo2004@gmail.com>
2025-08-01 16:57:57 +0100

    making update

--- src/Pages/AboutPage/AboutPages.jsx
+++ src/Pages/AboutPage/AboutPages.jsx
@@ -8,9 +8,9 @@ const AboutPages = () => {
     <Navbar/>
     <div>
       <h1>About Me</h1>
-      <p>My name is Aishat Adeyemi, and I am a dedicated frontend developer with a strong interest in creating functional and user-friendly web experiences. I specialize in building responsive websites using modern technologies such as HTML, CSS, JavaScript, and React.
-My journey in technology has been driven by a passion for problem-solving and continuous learning. Beyond coding, I enjoy exploring innovative design solutions and developing skills that enhance user interaction and digital accessibility.
-I am committed to leveraging my knowledge and creativity to develop impactful digital solutions that meet both aesthetic and functional needs</p>
+      <p>My name is Aishat Adeyemi, o and I am a dedicated frontend developer with a strong interest in creating functional and user-friendly web experiences. I specialize in building responsive websites using modern technologies such as HTML, CSS, JavaScript, and React.
+        My journey in technology has been driven by a passion for problem-solving and continuous learning. Beyond coding, I enjoy exploring innovative design solutions and developing skills that enhance user interaction and digital accessibility.
+       I am committed to leveraging my knowledge and creativity to develop impactful digital solutions that meet both aesthetic and functional needs</p>
     </div>
     </>
   )
